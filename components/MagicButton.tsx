d1d1caafdb8e1ea4ba7a6aa0aea7e228171b13dc
asadcodex <aslanrazasr@gmail.com>
2025-03-03 00:49:50 +0530

    use client

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 // history
 import React from "react";
 
--- components/ui/LayoutGrid.tsx
+++ components/ui/LayoutGrid.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
